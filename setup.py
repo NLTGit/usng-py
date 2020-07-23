8a69037f82def46f26b5b15ae89b93f32f2175ba
Carl Anderson <carl.anderson@vadose.org>
2020-07-23 17:58:32 -0400

    pivot to a module

---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import sys
+import os
+
+WD = '/'.join(sys.argv[0].split('/')[:-1])
+if WD > '':
+    os.chdir(WD)
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('usng-py/__init__.py', 'rt') as v:
+    for row in v:
+        version = None
+        if row.startswith('__version__ = '):
+            version = row[14:].strip().strip('"')
+            break
+    assert version is not None
+    with open('version.txt', 'wt') as f:
+        f.write(version)
+
+setup(
+    name='usng-py',
+    version=version,
+    description='Pure PYthon tool to compute USNG coordinates from Latitude/Longitude',
+    long_description=readme,
+    author='Carl Anderson',
+    author_email='carl.anderson@nltgis.com',
+    url='https://github.com/NLTGit/usng-py.git',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': ['usng=usng-py:main'],
+    }
+
+)
